8fc7bdee2f623ccad8af63ca9745fbdd8596c721
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-11 22:36:52 +0530

    Add Employee, Request Models

---
+++ day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/Models/Feedback.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int FeedbackId { get; set; }
+
+        public string FeedbackMessage { get; set; }
+
+        public int Rating { get; set; }
+
+        public DateTime PostedDate { get; set; } = System.DateTime.Now;
+
+        public int SolutionId { get; set; }
+
+        public RequestSolution Solution { get; set; }
+
+
+    }
+}
