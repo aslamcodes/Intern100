582f91d323978d634b85e39c53d1a4e8172c9fb8
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-12 20:35:06 +0530

    Implement FeedbackRepository

---
+++ day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerDAL/FeedbackRepository.cs
@@ -0,0 +1,43 @@
+using Models;
+
+namespace EmployeeTrackerDAL;
+
+public class FeedbackRepository: IRepository<int, Feedback>
+{
+    
+    private readonly RequestTrackerContext _context = new RequestTrackerContext();
+    public async Task<Feedback> Add(Feedback entity)
+    {
+        _context.Feedbacks.Add(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<Feedback> Update(Feedback entity)
+    {
+        _context.Feedbacks.Update(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<Feedback> Delete(int key)
+    {
+        var feedback = await Get(key);
+        _context.Feedbacks.Remove(feedback);
+        return feedback;
+    }
+
+    public async Task<Feedback> Get(int key)
+    {
+        var feedback = await _context.Feedbacks.FindAsync(key);
+        
+        return feedback;
+    }
+
+    public async Task<IList<Feedback>> GetAll()
+    {
+        var feetbacks = await _context.Feedbacks.ToListAsync();
+        
+        return feetbacks;
+    }
+}
