1dbe9b890aa9756abd359e8d97ad645af02f0158
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-12 12:06:34 +0530

    Move EntityNotFoundException.cs

---
+++ day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using Models.Enums;
+
+namespace EmployeeTrackerBL
+{
+    [Serializable]
+    public class EntityNotFoundException(EntityEnum entity) : Exception
+    {
+        public override string Message { get; } = $"Entity {entity} not found";
+    }
+}
