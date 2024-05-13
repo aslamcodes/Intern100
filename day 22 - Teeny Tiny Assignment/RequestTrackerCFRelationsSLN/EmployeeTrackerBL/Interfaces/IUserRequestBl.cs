77bc225133db7dc3be7c61e8386c00d06aded750
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-13 19:48:30 +0530

    Add Get All Request In RequestBL & Throw Exceptions

--- day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/Interfaces/IUserRequestBl.cs
+++ day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/Interfaces/IUserRequestBl.cs
@@ -8,8 +8,7 @@ public interface IUserRequestBl
 
     public Task<List<Request>> GetAllRequestForUser(Employee user);
 
-    public Task<Request> ViewSolutionsForUser();
-    
     public Task<Request> GetRequestById(int requestId);
     public Task<Request> MarkRequestClosed(Request request);
+    public Task<List<Request>> GetAllRequests();
 }
--- day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/UserRequestBl.cs
+++ day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/UserRequestBl.cs
@@ -1,50 +1,52 @@
+using EmployeeTrackerBL.Exceptions;
 using EmployeeTrackerDAL;
 using Models;
 using Models.Enums;
 
 namespace EmployeeTrackerBL;
 
-public class UserRequestBl: IUserRequestBl
+public class UserRequestBl : IUserRequestBl
 {
     private readonly IRepository<int, Request> _requestRepository = new RequestRepository(new RequestTrackerContext());
-   
-
 
     public async Task<Request> RaiseRequest(Request entity)
     {
         var request = await _requestRepository.Add(entity);
 
-        if (request == null)
-        {
-            throw new EntityNotFoundException(EntityEnum.Request);
-        }
-
-        return request;
+        return request ?? throw new CouldNotCreateEntityException(EntityEnum.Request);
     }
 
     public async Task<List<Request>> GetAllRequestForUser(Employee user)
     {
-       var requests=  await _requestRepository.GetAll();
+        var requests = await _requestRepository.GetAll();
 
-       return requests.Where(request => request.RequestRaisedBy == user.Id).ToList();
+        return requests != null
+            ? requests.Where(request => request.RequestRaisedBy == user.Id).ToList()
+            : throw new EntityNotFoundException(EntityEnum.Request);
     }
 
-    public Task<Request> ViewSolutionsForUser()
-    {
-        throw new NotImplementedException();
-    }
 
     public async Task<Request> GetRequestById(int requestId)
     {
         var request = await _requestRepository.Get(requestId);
-        
-        return request;
+
+        return request ?? throw new EntityNotFoundException(EntityEnum.Request);
     }
 
     public async Task<Request> MarkRequestClosed(Request request)
     {
         request.RequestStatus = "Closed";
+
         await _requestRepository.Update(request);
+
         return request;
+    }
+
+
+    public async Task<List<Request>> GetAllRequests()
+    {
+        var requests = await _requestRepository.GetAll();
+
+        return requests ?? throw new EntityNotFoundException(EntityEnum.Request);
     }
 }
