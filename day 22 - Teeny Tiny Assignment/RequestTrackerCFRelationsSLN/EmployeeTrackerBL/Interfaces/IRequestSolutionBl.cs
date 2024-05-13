faea9cd3bd7f39b646c9d6ae2e4faf1529a02d8f
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-13 20:53:48 +0530

    Add Accept Solution BL & throw exceptions on null

--- day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/Interfaces/IRequestSolutionBl.cs
+++ day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/Interfaces/IRequestSolutionBl.cs
@@ -4,6 +4,7 @@ namespace EmployeeTrackerBL;
 
 public interface IRequestSolutionBl
 {
+    public Task AcceptSolution(int solutionNumber);
     public Task<List<RequestSolution>> GetAllSolutions();
     public Task<List<RequestSolution>> GetAllSolutionsForUser(Employee authUser);
     public Task<RequestSolution> ProvideSolution(RequestSolution solution);
