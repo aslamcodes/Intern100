73916f8f51b3926534a7f925ef36356363fadbd8
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-13 19:48:41 +0530

    Add Could not create Exception

---
+++ day 22 - Teeny Tiny Assignment/RequestTrackerCFRelationsSLN/EmployeeTrackerBL/Exceptions/CouldNotCreateEntityException.cs
@@ -0,0 +1,18 @@
+﻿using Models.Enums;
+
+namespace EmployeeTrackerBL.Exceptions
+{
+    [Serializable]
+    public class CouldNotCreateEntityException : Exception
+    {
+
+        private readonly string _message;
+        public CouldNotCreateEntityException(EntityEnum entity)
+        {
+            _message = $"Could not create {entity} entity";
+        }
+
+        override public string Message => _message;
+
+    }
+}
