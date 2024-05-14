1b54bce1975d7ed367b048a3d35fb45cff14cd16
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-14 21:59:37 +0530

    Day24: DoctorClinic: add update Doctor controller & Get Doctor by spec and exp with query

--- day 24 Web API - Controllers & DI/DoctorClinicSLN/DoctorClinic/Controllers/DoctorController.cs
+++ day 24 Web API - Controllers & DI/DoctorClinicSLN/DoctorClinic/Controllers/DoctorController.cs
@@ -10,17 +10,42 @@ namespace DoctorClinic.Controllers
     public class DoctorController : ControllerBase
     {
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors(IDoctorService doctorService)
+        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctors([FromQuery] int? experience, [FromQuery] string? specialisation, IDoctorService doctorService)
         {
             try
             {
                 var doctors = await doctorService.GetDoctors();
+
+                if (experience.HasValue)
+                {
+                    doctors = doctors.Where(d => d.Experience >= experience);
+                }
+
+                if (!string.IsNullOrEmpty(specialisation))
+                {
+                    doctors = doctors.Where(d => d.Specialisation == specialisation);
+                }
 
                 return Ok(doctors);
             }
             catch (NoDoctorsFoundException)
             {
                 return NoContent();
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Doctor>> UpdateDoctor(Doctor doctor, IDoctorService doctorService)
+        {
+            try
+            {
+                var updatedDoctor = await doctorService.UpdateDoctor(doctor);
+
+                return Ok(updatedDoctor);
+            }
+            catch (Exception)
+            {
+                return NotFound();
             }
         }
     }
