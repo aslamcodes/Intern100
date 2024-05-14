28e0f7c39db96058c0c730733ed4abfc4f61cfc6
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-05-14 21:59:47 +0530

    Migrations

--- day 24 Web API - Controllers & DI/DoctorClinicSLN/DoctorClinic/Migrations/20240514141809_doctor-exp.Designer.cs
+++ day 24 Web API - Controllers & DI/DoctorClinicSLN/DoctorClinic/Migrations/20240514141809_doctor-exp.Designer.cs
@@ -3,6 +3,7 @@ using DoctorClinic.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoctorClinic.Migrations
 {
     [DbContext(typeof(DoctorClinicContext))]
-    partial class DoctorClinicContextModelSnapshot : ModelSnapshot
+    [Migration("20240514141809_doctor-exp")]
+    partial class doctorexp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ day 24 Web API - Controllers & DI/DoctorClinicSLN/DoctorClinic/Migrations/20240514141809_doctor-exp.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoctorClinic.Migrations
+{
+    /// <inheritdoc />
+    public partial class doctorexp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Experience",
+                table: "Doctors",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Experience",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Experience",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Doctors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Experience",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Experience",
+                table: "Doctors");
+        }
+    }
+}
