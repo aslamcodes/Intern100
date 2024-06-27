8c54f0f22f530c020c391919bef35a92f9ec667d
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-06-27 08:51:00 +0530

    Make every navbar responsive

--- 10th-June-2024 (Day 42)/Task/caber-frontend/js/navbarscript.js
+++ 10th-June-2024 (Day 42)/Task/caber-frontend/js/navbarscript.js
@@ -1,32 +1,51 @@
 (function () {
-  const navMenus = document.getElementsByClassName("cb-c-navbar__menu__item");
-  const mobileSidebarToggler = document.getElementById("js-mobile_menu");
-  const menus = {};
+  document.addEventListener("DOMContentLoaded", function () {
+    const navMenus = document.getElementsByClassName("cb-c-navbar__menu__item");
+    const mobileSidebarToggler = document.getElementById("js-mobile_menu");
+    const menus = {};
+    const currentRole = localStorage.getItem("role");
 
-  Array.from(navMenus).forEach((menu) => {
-    menus[menu.textContent] = menu.getAttribute("href");
-  });
+    Array.from(navMenus).forEach((menu) => {
+      // if the current role is driver
+      if (
+        menu.classList.contains("js-driver-role") &&
+        currentRole === "Driver"
+      ) {
+        menus[menu.textContent] = menu.getAttribute("href");
+      } else if (
+        menu.classList.contains("js-passenger-role") &&
+        currentRole === "Passenger"
+      ) {
+        menus[menu.textContent] = menu.getAttribute("href");
+      } else if (
+        !menu.classList.contains("js-driver-role") &&
+        !menu.classList.contains("js-passenger-role")
+      ) {
+        menus[menu.textContent] = menu.getAttribute("href");
+      }
+    });
 
-  const sidebar = document.getElementById("js-sidebar");
+    const sidebar = document.getElementById("js-sidebar");
 
-  if (sidebar === null) {
-    return;
-  }
+    if (sidebar === null) {
+      return;
+    }
 
-  // add the menus to sidebar
-  for (const menu in menus) {
-    const a = document.createElement("a");
-    a.textContent = menu;
-    a.setAttribute("href", menus[menu]);
-    sidebar.appendChild(a);
-  }
+    // add the menus to sidebar
+    for (const menu in menus) {
+      const a = document.createElement("a");
+      a.textContent = menu;
+      a.setAttribute("href", menus[menu] ?? "#");
+      sidebar.appendChild(a);
+    }
 
-  mobileSidebarToggler.addEventListener("click", function () {
-    sidebar.classList.toggle("open");
-  });
+    mobileSidebarToggler.addEventListener("click", function () {
+      sidebar.classList.toggle("open");
+    });
 
-  const sidebarClose = document.getElementById("js-sidebar-close");
-  sidebarClose.addEventListener("click", function () {
-    sidebar.classList.remove("open");
+    const sidebarClose = document.getElementById("js-sidebar-close");
+    sidebarClose.addEventListener("click", function () {
+      sidebar.classList.remove("open");
+    });
   });
 })();
