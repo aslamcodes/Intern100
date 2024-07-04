53912fd8a95430b0a328916015249abf1a45c4fd
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-07-04 14:21:12 +0530

    July 4 Updates

---
+++ 04-July-2024 Docker/Session/commands.sh
@@ -0,0 +1,17 @@
+docker pull ubuntu
+
+docker images
+
+docker run -it ubuntu
+
+docker run -it --name linux_ubuntu ubuntu
+
+docker ps 
+
+docker ps -a
+
+docker rm conatiner_id
+
+docker rmi image_id
+
+docker system prune -a
---
+++ 04-July-2024 Docker/Session/ubuntu_commands.sh
@@ -0,0 +1,7 @@
+mkdir folder
+touch file.txt
+nano file.txt
+head file.txt
+rm file.txt
+rm -r folder
+exit
